d248491ad84da0f989a5115901336ed9836baee4
dani09 <daniel@dani09.de>
2018-08-02 19:33:02 +0200

    Added MovieDownloader and saveMovieData()

---
+++ src/main/java/de/dani09/moviedownloader/MovieDownloader.scala
@@ -0,0 +1,57 @@
+package de.dani09.moviedownloader
+
+import java.io.{BufferedReader, File, FileOutputStream, InputStreamReader}
+import java.net.URL
+import java.nio.file.Path
+
+import de.dani09.moviedownloader.config.Config
+import me.tongfei.progressbar.{ProgressBarBuilder, ProgressBarStyle}
+
+class MovieDownloader(config: Config) {
+
+  def saveMovieData(destination: Path, downloadUrl: URL): Unit = {
+    var file: FileOutputStream = null
+    var reader: BufferedReader = null
+
+
+    try {
+      file = new FileOutputStream(new File(destination.toUri))
+
+      val connection = downloadUrl.openConnection()
+      val fullSize = connection.getContentLengthLong
+      val input = connection.getInputStream
+
+      val pb = new ProgressBarBuilder()
+        .setTaskName("Downloading Movie Data")
+        .setStyle(ProgressBarStyle.ASCII)
+        .setUnit("MB", 1048576)
+        .setInitialMax(fullSize)
+        .setUpdateIntervalMillis(100)
+        .build()
+
+      reader = new BufferedReader(new InputStreamReader(input))
+
+      val data = new Array[Byte](1024)
+      var count = 0
+      while (count != -1) {
+        count = input.read(data, 0, 1024)
+        if (count != -1) {
+          file.write(data)
+          pb.stepBy(count)
+        } else {
+          pb.close()
+        }
+      }
+
+    } finally {
+      if (reader != null) {
+        reader.close()
+      }
+
+      if (file != null) {
+        file.close()
+      }
+    }
+  }
+
+}
