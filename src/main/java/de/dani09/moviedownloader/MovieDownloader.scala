ee700d6d3f6e8c49d8d1034d3b229a957d76cae8
dani09 <daniel@dani09.de>
2018-08-04 18:58:54 +0200

    Implemented Download Movie

--- src/main/java/de/dani09/moviedownloader/MovieDownloader.scala
+++ src/main/java/de/dani09/moviedownloader/MovieDownloader.scala
@@ -15,48 +15,11 @@ import scala.collection.JavaConverters._
 
 class MovieDownloader(config: Config) {
 
-  def saveMovieData(destination: Path, downloadUrl: URL): Unit = {
-    var file: FileOutputStream = null
-    var reader: BufferedReader = null
+  def saveMovieData(destination: Path, downloadUrl: URL): Unit = downloadFile(destination, downloadUrl, "Movie Data")
 
-    try {
-      file = new FileOutputStream(new File(destination.toUri))
-
-      val connection = downloadUrl.openConnection()
-      val fullSize = connection.getContentLengthLong
-      val input = connection.getInputStream
-
-      val pb = new ProgressBarBuilder()
-        .setTaskName("Downloading Movie Data")
-        .setStyle(ProgressBarStyle.ASCII)
-        .setUnit("MB", 1048576)
-        .setInitialMax(fullSize)
-        .setUpdateIntervalMillis(100)
-        .build()
-
-      reader = new BufferedReader(new InputStreamReader(input))
-
-      val data = new Array[Byte](1024)
-      var count = 0
-      while (count != -1) {
-        count = input.read(data, 0, 1024)
-        if (count != -1) {
-          file.write(data, 0, count)
-          pb.stepBy(count)
-        } else {
-          pb.close()
-        }
-      }
-
-    } finally {
-      if (reader != null) {
-        reader.close()
-      }
-
-      if (file != null) {
-        file.close()
-      }
-    }
+  def isMovieAlreadyDownloaded(movie: Movie): Boolean = {
+    val path: Path = getMovieSavePath(movie)
+    path.toFile.exists()
   }
 
   //noinspection SpellCheckingInspection
@@ -88,12 +51,59 @@ class MovieDownloader(config: Config) {
       .toList
   }
 
-  def isMovieAlreadyDownloaded(movie: Movie): Boolean = {
-    val path: Path = getMoviePath(movie)
-    path.toFile.exists()
+  def downloadMovie(movie: Movie): Unit = {
+    val destinationPath = getMovieSavePath(movie)
+    val name = s"${movie.tvChannel} --> ${movie.seriesTitle} --> ${movie.episodeTitle}"
+
+    println(s"Downloading $name")
+    downloadFile(destinationPath, movie.downloadUrl, name)
   }
 
-  private def getMoviePath(movie: Movie) = {
+  private def downloadFile(destination: Path, downloadUrl: URL, nameForProgress: String): Unit = {
+    var file: FileOutputStream = null
+    var reader: BufferedReader = null
+
+    try {
+      file = new FileOutputStream(new File(destination.toUri))
+
+      val connection = downloadUrl.openConnection()
+      val fullSize = connection.getContentLengthLong
+      val input = connection.getInputStream
+
+      val pb = new ProgressBarBuilder()
+        .setTaskName(s"Downloading $nameForProgress")
+        .setStyle(ProgressBarStyle.ASCII)
+        .setUnit("MB", 1048576)
+        .setInitialMax(fullSize)
+        .setUpdateIntervalMillis(100)
+        .build()
+
+      reader = new BufferedReader(new InputStreamReader(input))
+
+      val data = new Array[Byte](1024)
+      var count = 0
+      while (count != -1) {
+        count = input.read(data, 0, 1024)
+        if (count != -1) {
+          file.write(data, 0, count)
+          pb.stepBy(count)
+        } else {
+          pb.close()
+        }
+      }
+
+    } finally {
+      if (reader != null) {
+        reader.close()
+      }
+
+      if (file != null) {
+        file.close()
+      }
+    }
+  }
+
+  private def getMovieSavePath(movie: Movie) = {
     val date = movie.releaseDate
     val calendar = Calendar.getInstance()
     calendar.setTime(date)
@@ -104,11 +114,6 @@ class MovieDownloader(config: Config) {
     val pathString = s"${config.downloadDirectory}/${movie.tvChannel}/${movie.seriesTitle}/${movie.episodeTitle}-$dateString.$fileExtension"
     Paths.get(pathString)
   }
-
-  /*
-  def downloadMovie(): Unit = {
-
-  }*/
 
   private def getFileExtension(url: URL): String = {
     url.getPath
