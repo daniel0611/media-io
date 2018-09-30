37a113c4d7ff995f4aded513264aad297d392e15
dani09 <daniel@dani09.de>
2018-09-30 17:05:37 +0200

    Documented DownloadedMovies

--- src/main/java/de/dani09/moviedownloader/config/DownloadedMovies.scala
+++ src/main/java/de/dani09/moviedownloader/config/DownloadedMovies.scala
@@ -11,10 +11,15 @@ import scala.io.Source
 
 class DownloadedMovies(private val movies: ListBuffer[Movie]) {
 
-  def this(movies: List[Movie]) = this(movies.to[ListBuffer])
+  def this(movies: List[Movie]) = this(movies.to[ListBuffer]) // converts List to ListBuffer in order to use an List as an argument
 
-  def this() = this(ListBuffer[Movie]())
+  def this() = this(ListBuffer[Movie]()) // empty constructor
 
+  /**
+    * Serializes the DownloadedMovies list by converting it to json and writing it to the File "downloaded-movies.json"
+    *
+    * @param config config is needed to get the Path where the file will be saved
+    */
   def serialize(config: Config): Unit = {
     val file = DownloadedMovies.getDownloadedMoviesFile(config)
 
@@ -25,20 +30,42 @@ class DownloadedMovies(private val movies: ListBuffer[Movie]) {
     fw.close()
   }
 
+  /**
+    * Converts the List of DownloadedMovies into an JSONArray
+    *
+    * @return returns this List as an JSONArray
+    */
   def toJson: JSONArray = {
     movies
       .groupBy(_.downloadUrl) // this...
       .map(_._2.head) // ... and this filters duplicates with the same download url
-      .map(_.toJson)
-      .foldLeft(new JSONArray())((arr, x) => arr.put(x))
+      .map(_.toJson) // List[Movie] -> List[JSONObject]
+      .foldLeft(new JSONArray())((arr, x) => arr.put(x)) // put JSONObjects into JSONArray // List[JSONObject] -> JSONArray
   }
 
+  /**
+    * Adds the Movie to the List
+    *
+    * @param m the Movie you want to add
+    */
   def addMovie(m: Movie): Unit = movies += m
 
+  /**
+    * Get all Movies as an Scala ListBuffer
+    *
+    * @return all Movies
+    */
   def getMovies: ListBuffer[Movie] = movies
 }
 
 object DownloadedMovies {
+
+  /**
+    * deSerializes an DownloadedMovies List by parsing the file in which it is stored and parsing this json
+    *
+    * @param config config is needed to get the Path where the file is located
+    * @return returns an instance of DownloadedMovies with the parsed Data
+    */
   def deserialize(config: Config): DownloadedMovies = {
     val file = getDownloadedMoviesFile(config)
     if (!file.exists()) new DownloadedMovies()
@@ -49,16 +76,16 @@ object DownloadedMovies {
 
       val movies = (for (i <- 0 until arr.length()) yield i)
         .par
-        .map(index => arr.getJSONObject(index))
-        .map(j => {
-          try {
-            Movie.fromJson(j)
-          } catch {
-            case _: JSONException => null
-          }
-        })
-        .filter(_ != null)
-        .toList
+        .map(index => arr.getJSONObject(index)) // get JSONObjects by index
+        .map(json => { // Parse Movies with JSONObjects
+        try {
+          Movie.fromJson(json)
+        } catch {
+          case _: JSONException => null // will be removed by filter afterwards
+        }
+      }) // ParSeq[JSONObject] -> ParSeq[Movie]
+        .filter(_ != null) // filter those out which did threw an JSONException
+        .toList // ParSeq[Movies] -> List[Movies]
 
       new DownloadedMovies(movies)
     } catch {
@@ -68,6 +95,12 @@ object DownloadedMovies {
     }
   }
 
+  /**
+    * get the Path where the DownloadedMovies File is located
+    *
+    * @param config config is needed to get the DownloadDirectory
+    * @return returns the Path where the File is
+    */
   private def getDownloadedMoviesFile(config: Config): File = Paths
     .get(config.downloadDirectory.toString, "./downloaded-movies.json")
     .toFile
