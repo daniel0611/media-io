4b9fb4099496a7114472c1193417405380e8a33a
dani09 <daniel@dani09.de>
2018-10-02 18:37:16 +0200

    Converted Movie to case class, added Movie.getRelativeSavePath and replacing url in WebFrontendServlet with the actual url that the file will be served on by nginx

--- src/main/java/de/dani09/moviedownloader/web/WebFrontendServlet.scala
+++ src/main/java/de/dani09/moviedownloader/web/WebFrontendServlet.scala
@@ -1,6 +1,9 @@
 package de.dani09.moviedownloader.web
 
+import java.net.{URL, URLEncoder}
+
 import de.dani09.moviedownloader.config.{CLIConfig, Config, DownloadedMovies}
+import de.dani09.moviedownloader.data.Movie
 import org.scalatra.ScalatraServlet
 import org.slf4j.LoggerFactory
 
@@ -9,8 +12,23 @@ class WebFrontendServlet(conf: Config, cli: CLIConfig) extends ScalatraServlet {
   private val logger = LoggerFactory.getLogger(getClass)
 
   get("/getMovies") {
-    val movies = DownloadedMovies.deserialize(conf)
+    val movies = DownloadedMovies
+      .deserialize(conf)
+      .getMovies
+      .map(m => m.copy(downloadUrl = getMovieUrl(m)))
 
-    movies.toJson
+    new DownloadedMovies(movies).toJson
+  }
+
+  def getMovieUrl(m: Movie): URL = {
+    val path = m.getRelativeSavePath.toString
+
+    val encoded = path
+      .split("/")
+      .map(u => URLEncoder.encode(u, "utf8"))
+      .map(_.replaceAll("\\+", "%20"))
+      .reduceLeft((a, b) => a + "/" + b)
+
+    new URL(fullUrl("/data/" + encoded, includeServletPath = false))
   }
 }
