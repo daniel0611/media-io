84ae78ee8090878b187c74431c328e1293d9c415
dani09 <daniel@dani09.de>
2018-10-06 15:46:54 +0200

    Added more Api endpoints

--- src/main/java/de/dani09/moviedownloader/web/WebFrontendServlet.scala
+++ src/main/java/de/dani09/moviedownloader/web/WebFrontendServlet.scala
@@ -4,6 +4,7 @@ import java.net.{URL, URLEncoder}
 
 import de.dani09.moviedownloader.config.{CLIConfig, Config, DownloadedMovies}
 import de.dani09.moviedownloader.data.Movie
+import org.json.{JSONArray, JSONObject}
 import org.scalatra.ScalatraServlet
 import org.slf4j.LoggerFactory
 
@@ -12,6 +13,10 @@ class WebFrontendServlet(conf: Config, cli: CLIConfig) extends ScalatraServlet {
   private val logger = LoggerFactory.getLogger(getClass)
   private implicit val servlet: ScalatraServlet = this
 
+  before() {
+    contentType = "application/json"
+  }
+
   get("/getMovies") {
     val movies = DownloadedMovies
       .deserialize(conf)
@@ -19,6 +24,63 @@ class WebFrontendServlet(conf: Config, cli: CLIConfig) extends ScalatraServlet {
       .map(m => m.copy(downloadUrl = WebFrontendServlet.getMovieUrl(m)))
 
     new DownloadedMovies(movies).toJson
+  }
+
+  get("/getTvChannels") {
+    // TODO comment this
+    DownloadedMovies
+      .deserialize(conf)
+      .getMovies
+      .map(_.tvChannel)
+      .map(_.toUpperCase)
+      .groupBy(identity)
+      .filter(_._2.nonEmpty)
+      .keys
+      .foldLeft(new JSONArray())((arr, key) => arr.put(key))
+      .toString
+  }
+
+  get("/getSeries") {
+    val channel = params.getOrElse("tvChannel", "").toUpperCase()
+
+    DownloadedMovies
+      .deserialize(conf)
+      .getMovies
+      .filter(m => channel == "" | channel == m.tvChannel) // all if no channel was specified and otherwise check if channel is the same
+      .map(_.toJson)
+      .foldLeft(new JSONArray())((arr, movie) => arr.put(movie))
+      .toString
+  }
+
+  get("/getEpisodes") {
+    val series = params.getOrElse("series", "").toLowerCase
+
+    if (series == "") {
+      status = 400
+      "Please specify a series"
+    } else {
+      DownloadedMovies
+        .deserialize(conf)
+        .getMovies
+        .filter(_.seriesTitle.toLowerCase == series)
+        .map(_.toJson)
+        .foldLeft(new JSONArray())((arr, movie) => arr.put(movie))
+        .toString
+    }
+  }
+
+  get("/getOverView") {
+    // FIXME document this and make it easier to understand!!!!!!!
+    DownloadedMovies
+      .deserialize(conf)
+      .getMovies
+      .groupBy(_.tvChannel)
+      .mapValues(_.groupBy(_.seriesTitle))
+      .mapValues(_.mapValues(_.map(_.toJson)))
+      .mapValues(_.mapValues(_.foldLeft(new JSONArray())((arr, item) => arr.put(item))))
+      .mapValues(_.foldLeft(new JSONObject())((json, item) => json.put(item._1, item._2)))
+      .foldLeft(new JSONObject())((json, item) => json.put(item._1, item._2))
+      .toString
   }
 }
 
